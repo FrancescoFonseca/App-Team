7111c09fd816ec2c0f599de93162acfc3ec49ae4
FrancescoFonseca <francescofonseca@hotmail.it>
2026-05-29 13:24:21 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,39 @@
-const cacheName = 'fonseca-v2';
-const assets = ['./', './index.html', './manifest.json'];
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(assets);
+const CACHE_NAME = 'dr-fonseca-cache-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installazione e attivazione immediata senza attendere
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+// Cancella le vecchie cache all'attivazione
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Strategia Network First: cerca prima online, se non c'è rete usa la cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
